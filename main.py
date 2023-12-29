2b4b4d5628620747ab40791a8d2b941ea3916ef4
An72ty <An72ty@gmail.com>
2023-12-29 10:34:36 +0500

    added /avatar and /user_info command

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import disnake
 from disnake.ext import commands
 
 
-bot = commands.Bot()
+bot = commands.Bot(test_guilds=[1113102018991620228, 972183553775382530])
 
 
 @bot.event
@@ -12,7 +12,25 @@ async def on_ready():
     print('The bot is ready!')
 
 
-load_dotenv('token.env')
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+# @bot.slash_command(guild_ids=[1113102018991620228])
+# async def ping(inter: disnake.ApplicationCommandInteraction):
+#     await inter.response.send_message("Pong!")
 
-bot.run(BOT_TOKEN)
+bot.load_extension("cogs.public_commands")
+    
+    
+# @bot.slash_command()
+# async def server_info(inter: disnake.ApplicationCommandInteraction):
+#     """Returns the info about the server"""
+#     await inter.response.send_message(f"Server name: {inter.guild.name}\nTotal members: {inter.guild.member_count}")
+
+
+# @bot.slash_command()
+# async def user_info(inter: disnake.ApplicationCommandInteraction):
+#     """Returns the info about the user"""
+#     await inter.response.send_message(f"User name: {inter.author}\nUser ID: {inter.author.id}")
+
+
+load_dotenv('token.env')
+
+bot.run(os.getenv('BOT_TOKEN'))
